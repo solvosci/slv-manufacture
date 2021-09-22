c79daf6817eda5b199aa81c8d7d9d5967fedfb3b
David Alonso // Solvos <david.alonso@solvos.es>
2021-09-22 14:20:12 +0200

    [ADD] mrp_unbuild_qc

---
+++ mrp_unbuild_qc/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "MRP Unbuild Quality Control",
+    "summary": """
+        Enable inspection of product movements from unbuild order
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://github.com/solvosci/slv-manufacture",
+    "depends": ["quality_control_mrp_oca"],
+    "data": [
+        "views/mrp_unbuild_views.xml",
+        "views/qc_inspection_views.xml",
+        "views/stock_move_line_views.xml"
+    ],
+    'installable': True,
+}
