913fcfacb4b0bfcfea07997c52d9adcc86717fb1
David Alonso // Solvos <david.alonso@solvos.es>
2021-09-23 17:35:05 +0200

    [ADD] mrp_unbuild_valuation

---
+++ mrp_unbuild_valuation/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "MRP Unbuild Valuation",
+    "summary": """
+        Adds valuation view for done unbuilds
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://github.com/solvosci/slv-manufacture",
+    "depends": ["mrp", "stock_account"],
+    "data": [
+        "views/mrp_unbuild_views.xml",
+    ],
+    'installable': True,
+}
