d13935a2bc88bbf6b73f570bda419f1f104c51bd
David Alonso // Solvos <david.alonso@solvos.es>
2021-10-07 11:26:46 +0200

    [ADD] mrp_unbuild_advanced

---
+++ mrp_unbuild_advanced/models/stock_move.py
@@ -0,0 +1,14 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _check_safe_removal(self):
+        """
+        Override when required
+        """
+        pass
