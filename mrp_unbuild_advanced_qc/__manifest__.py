25f0d886f7c91fbf0d44209b915bb6285fd4ea3a
David Alonso // Solvos <david.alonso@solvos.es>
2021-10-07 11:28:46 +0200

    [ADD] mrp_unbuild_advanced_qc

---
+++ mrp_unbuild_advanced_qc/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "MRP Unbuild Advanced additions - Quality inspection bridge",
+    "summary": """
+        Links Advanced Unbuilds with Quality Inspections
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://github.com/solvosci/slv-manufacture",
+    "depends": ["mrp_unbuild_advanced", "mrp_unbuild_qc"],
+    "data": [],
+    'installable': True,
+}
