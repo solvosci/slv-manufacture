2b0dd1e547a32fb968facb5bf6fd2b902b01393f
David Alonso // Solvos <david.alonso@solvos.es>
2021-10-07 11:28:46 +0200

    [ADD] mrp_unbuild_advanced_qc

---
+++ mrp_unbuild_advanced_qc/models/stock_move.py
@@ -0,0 +1,21 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+
+from odoo import _, models
+from odoo.exceptions import ValidationError
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _check_safe_removal(self):
+        super()._check_safe_removal()
+        inspections = self.env["qc.inspection"].search([
+            ("unbuild_id", "=", self.unbuild_id.id)
+        ])
+        if inspections:
+            raise ValidationError(_(
+                "There are %d inspections(s) linked to the current generated"
+                " moves. Please remove them before backing unbuild to draft")
+                % len(inspections)
+            )
