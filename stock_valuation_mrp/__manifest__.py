ff86a69e9617c08194a5c777407c698651155150
David Alonso // Solvos <david.alonso@solvos.es>
2022-07-12 18:46:52 +0200

    [ADD] stock_valuation_mrp

---
+++ stock_valuation_mrp/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Stock Valuation MRP",
+    "summary": """
+        Link between Stock Valuation and MRP modules
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "stock",
+    "website": "https://github.com/solvosci/slv-stock",
+    "depends": ["stock_valuation", "mrp_unbuild_advanced"],
+    "data": [],
+    'installable': True,
+}
