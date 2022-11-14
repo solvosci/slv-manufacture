dce42c4449ca7cb8c83917908f90595883e8e069
resua <adrian.resua@solvos.es>
2022-11-14 10:31:02 +0100

    [ADD] mrp_project

---
+++ mrp_project/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Manufacture and Project bind",
+    "summary": """
+    For bind productions to projects through 2 fields, Many2one and One2many respectively. One project has many manufactures. 
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "11.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://github.com/solvosci/slv-manufacture",
+    "depends": ["mrp", "project"],
+    "data": [
+        "views/project_project_views.xml",
+        "views/mrp_production_views.xml",
+    ],
+    "installable": True,
+}
