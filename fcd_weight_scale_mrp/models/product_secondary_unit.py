a6d45aaa34e8b8c199f4927ca929de1c00015304
Christian Santamaria <christian.santamaria@solvos.es>
2023-01-03 17:40:21 +0100

    [IMP] fcd_weight_scale_mrp: add protection to secondary uom with factor 0

--- fcd_weight_scale_mrp/models/product_secondary_unit.py
+++ fcd_weight_scale_mrp/models/product_secondary_unit.py
@@ -9,7 +9,21 @@ class ProductSecondaryUnit(models.Model):
     _inherit = "product.secondary.unit"
 
     @api.model
+    def create(self, values):
+        if self.env['product.template'].browse(values['product_tmpl_id']).production_secondary_uom_id and values['factor'] == 0:
+            raise ValidationError(_('Cannot create another secondary unit of measure with factor 0.00 !'))
+        return super(ProductSecondaryUnit, self).create(values)
+
+    def write(self, values):
+        for record in self:
+            if record.id == record.product_tmpl_id.production_secondary_uom_id.id:
+                raise ValidationError(_('Cannot edit secondary unit of measure with factor 0.00 !'))
+            if record.product_tmpl_id.production_secondary_uom_id and values['factor'] == 0:
+                raise ValidationError(_('Cannot create another secondary unit of measure with factor 0.00 !'))
+        super(ProductSecondaryUnit, self).write(values)
+
     def unlink(self):
-        if self.id == self.product_tmpl_id.production_secondary_uom_id.id:
-            raise ValidationError(_('You should not unlink the secondary unit used on production!'))
-        return super().unlink()
+        for record in self:
+            if record.id == record.product_tmpl_id.production_secondary_uom_id.id:
+                raise ValidationError(_('Cannot delete secondary unit of measure with factor 0.00 !'))
+        super(ProductSecondaryUnit, self).unlink()
