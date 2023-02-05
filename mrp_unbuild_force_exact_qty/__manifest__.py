5e64dfc29ec6a4535ed7589e20478f35037c14bc
David Alonso // Solvos <david.alonso@solvos.es>
2023-02-05 19:16:04 +0100

    [ADD] mrp_unbuild_force_exact_qty

---
+++ mrp_unbuild_force_exact_qty/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "MRP Unbuild Force Exact Quantity",
+    "summary": """
+        For those unbuilds with homogeneus consumed and produced products,
+        forces that the sum of qty consumed equals to total produced
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://github.com/solvosci/slv-manufacture",
+    "depends": ["mrp"],
+    'installable': True,
+}
