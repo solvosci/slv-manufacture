cf8bf8ff890fd82d88b00ac14e34d2513d8c8403
Christian Santamaria <christian.santamaria@solvos.es>
2023-09-06 14:58:29 +0200

    [FIX] fcd_weight_scale_mrp: slowness in obtaining purchase lines on scale

--- fcd_weight_scale_mrp/__manifest__.py
+++ fcd_weight_scale_mrp/__manifest__.py
@@ -8,7 +8,7 @@
     'author': "Solvos",
     'website': "http://www.solvos.com",
     'category': 'Weight',
-    'version': '14.0.1.5.0',
+    'version': '14.0.1.5.1',
     'depends': [
         "mrp",
         "fcd_purchase_order",
