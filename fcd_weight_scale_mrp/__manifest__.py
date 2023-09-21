fa0b014697415804a3fefc6be933d241f81a6baa
Christian Santamaria <christian.santamaria@solvos.es>
2023-09-21 09:36:58 +0200

    [IMP] fcd_weight_scale_mrp: update today filter and new group by warehouse and partner

--- fcd_weight_scale_mrp/__manifest__.py
+++ fcd_weight_scale_mrp/__manifest__.py
@@ -8,7 +8,7 @@
     'author': "Solvos",
     'website': "http://www.solvos.com",
     'category': 'Weight',
-    'version': '14.0.1.5.1',
+    'version': '14.0.1.6.0',
     'depends': [
         "mrp",
         "fcd_purchase_order",
