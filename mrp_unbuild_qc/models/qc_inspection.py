be4421691cb4416579bc2531a90d5af9a0ae7c7c
David Alonso // Solvos <david.alonso@solvos.es>
2024-04-10 13:01:23 +0200

    [IMP] mrp_unbuild_qc: partial coverage for inspections only linked to the unbuild, not stock_move
    With this improvement, it's possible to create an inspection linked to
    an unbuild without selection a stock move.
    This could be achieved from backend, because in frontend is still not
    possible to select the unbuild (only a move).

--- mrp_unbuild_qc/models/qc_inspection.py
+++ mrp_unbuild_qc/models/qc_inspection.py
@@ -1,7 +1,7 @@
 # © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class QcInspection(models.Model):
@@ -11,3 +11,11 @@ class QcInspection(models.Model):
         comodel_name="mrp.unbuild",
         readonly=True,
     )
+
+    @api.depends("unbuild_id")
+    def _compute_product_id(self):
+        super()._compute_product_id()
+        for inspection in self.filtered(
+            lambda x: x.unbuild_id and not x.object_id
+        ):
+            inspection.product_id = inspection.unbuild_id.product_id
--- mrp_unbuild_qc/readme/DESCRIPTION.rst
+++ mrp_unbuild_qc/readme/DESCRIPTION.rst
@@ -1,1 +1,7 @@
-Enable inspection of product movements from unbuild order
+Enable inspection of product movements from unbuild order.
+
+Also adds the possibility to create an inspection directly linked to an
+unbuild. This couldn't be achieved using the frontend (it's not possible to
+manually select the unbuild), but it could be useful for e.ge. backend
+inspections import.
+
