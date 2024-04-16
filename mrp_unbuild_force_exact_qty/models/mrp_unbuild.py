ea42b4d383c5c2033b526448cf07109339df295f
David Alonso // Solvos <david.alonso@solvos.es>
2024-04-16 09:28:33 +0200

    [IMP] mrp_unbuild_force_exact_qty: change exact quantity policy to always same UoM-categ products
    With this improvement, quantity check is always made, even if there are
    produced lines with different UoM category. Those moves are ignored
    anyway. In previous version, quantity check was disabled in that case.
    
    Produced vs consumed moves filtering is improved, now are separated
    depending on destination location usage, instead of product definition

--- mrp_unbuild_force_exact_qty/models/mrp_unbuild.py
+++ mrp_unbuild_force_exact_qty/models/mrp_unbuild.py
@@ -11,28 +11,28 @@ class MrpUnbuild(models.Model):
 
     def action_unbuild(self):
         ret = super().action_unbuild()
-        # We'll only check quantities if every consumed and produced move
-        #  belongs to the same UoM cateogory. Other operation should be
-        #  inconsistent
-        uom_categs = self.produce_line_ids.mapped("product_uom_category_id")
-        if len(uom_categs) == 1:
-            move_consumed = self.produce_line_ids.filtered(
-                lambda x: x.product_id == self.product_id
+        # We'll only check quantities that belong to the same UoM category.
+        #  Other operation should be inconsistent
+        line_ids = self.produce_line_ids.filtered(
+            lambda x: x.product_uom_category_id == self.product_id.uom_id.category_id
+        )
+        move_consumed = line_ids.filtered(
+            lambda x: x.location_dest_id.usage == "production"
+        )
+        move_produced = (line_ids - move_consumed)
+        qty_consumed = sum(move_consumed.mapped("quantity_done"))
+        qty_produced = sum(
+            move.product_uom._compute_quantity(
+                move.quantity_done, self.product_uom_id
             )
-            move_produced = (self.produce_line_ids - move_consumed)
-            qty_consumed = sum(move_consumed.mapped("quantity_done"))
-            qty_produced = sum(
-                move.product_uom._compute_quantity(
-                    move.quantity_done, self.product_uom_id
-                )
-                for move in move_produced
+            for move in move_produced
+        )
+        if not float_is_zero(
+            qty_consumed - qty_produced,
+            precision_rounding=self.product_uom_id.rounding
+        ):
+            raise ValidationError(
+                _("Unbuild and generated quantities don't match (%.3f != %.3f)")
+                % (qty_consumed, qty_produced)
             )
-            if not float_is_zero(
-                qty_consumed - qty_produced,
-                precision_rounding=self.product_uom_id.rounding
-            ):
-                raise ValidationError(
-                    _("Unbuild and generated quantities don't match (%.3f != %.3f)")
-                    % (qty_consumed, qty_produced)
-                )
         return ret
--- mrp_unbuild_force_exact_qty/readme/DESCRIPTION.rst
+++ mrp_unbuild_force_exact_qty/readme/DESCRIPTION.rst
@@ -1,2 +1,2 @@
-For those unbuilds with homogeneus consumed and produced products, forces
-that the sum of qty consumed equals to total produced.
+For same-UoM category produced products, forces that the sum of qty consumed
+equals to total produced.
