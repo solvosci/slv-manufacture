2109bf8584077f72eac7ece2c75c7da5a892e710
Adrian Resua Vidal <resuavidal@gmail.com>
2024-04-23 17:34:44 +0200

    [FIX] mrp_unbuild_view_improves: Add bom code to calendar view

---
+++ mrp_unbuild_view_improves/models/mrp_unbuild.py
@@ -0,0 +1,15 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+
+from odoo import _, api, models, fields
+from odoo.exceptions import ValidationError
+
+
+class MrpUnbuild(models.Model):
+    _inherit = "mrp.unbuild"
+
+    bom_code = fields.Char(
+        string="Bom Code",
+        readonly=True,
+        related="bom_id.code",
+    )
