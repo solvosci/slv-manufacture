c29e7bf1520ca7e7e7b1d665f8e4a8f00e7236fc
Christian Santamaria <christian.santamaria@solvos.es>
2024-11-14 12:02:14 +0100

    [ADD] mrp_project_analytic_purchase

---
+++ mrp_project_analytic_purchase/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "MRP Project Analytic Purchase",
+    "summary": """
+        For those MRPs linked to purchase, when account moves are generated,
+        project analytic account is automatically set
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://github.com/solvosci/slv-manufacture",
+    "depends": ["mrp_project_analytic", "purchase"],
+    "data": [],
+    "installable": True,
+}
---
+++ mrp_project_analytic_purchase/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+For those MRPs linked to purchase, when account moves are generated,
+project analytic account is automatically set
