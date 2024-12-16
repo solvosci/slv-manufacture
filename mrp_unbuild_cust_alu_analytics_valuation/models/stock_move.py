f9065d810fb159d6913dafb7691e433985b82bfb
David Alonso // Solvos <david.alonso@solvos.es>
2024-12-16 18:57:23 +0100

    [IMP] mrp_unbuild_cust_alu_analytics_valuation: output pickings valuation data
    When an output picking is done some valuation data is added when stock
    moves are linked to unbuilds via used lots. Otherwise, valuation
    info will use average price for the proper date, warehouse and product.

--- mrp_unbuild_cust_alu_analytics_valuation/models/stock_move.py
+++ mrp_unbuild_cust_alu_analytics_valuation/models/stock_move.py
@@ -28,3 +28,47 @@ class StockMove(models.Model):
         if quant_total and quant_total[0].disabled_mrp_unbuild_valuation:
             return 0.0
         return quantity
+    
+    def _get_move_cost(self):
+        """
+        Obtains move cost based on its move lines.
+        When a move line uniquely linked to an unbuild cost is obtained
+        from such unbuild; if not, from valuation layer
+        """
+        self.ensure_one()
+        cost = 0.0
+        for ml in self.move_line_ids.filtered(lambda x: x.state == "done"):
+            unbuild_id = ml.move_id._get_move_line_cost_unbuild_ids(ml)
+            if len(unbuild_id) == 1:
+                cost += unbuild_id.cost_unit_price * ml.qty_done
+            else:
+                # TODO if there's more than one valuation layer?
+                cost += ml.qty_done * (
+                    self.stock_valuation_layer_ids
+                    and self.stock_valuation_layer_ids[0].unit_cost
+                    or 0.0
+                )
+
+        return cost
+    
+    def _get_move_line_cost_unbuild_ids(self, ml):
+        # TODO move to stock_move_line.py, and ml should be self
+        return ml.lot_id and ml.search([
+            ("lot_id", "=", ml.lot_id.id),
+            ("move_id.unbuild_id", "!=", False),
+            ("location_id.usage", "=", "production"),
+            ("state", "=", "done"),
+        ]).move_id.unbuild_id or self.env["mrp.unbuild"]
+        
+    def _get_move_income_price_unit(self):
+        # TODO it should be price AND product_uom_id
+        self.ensure_one()
+        sale_line_id = (
+            "sale_line_id" in self
+            and self["sale_line_id"]
+            or False
+        )
+        if sale_line_id:
+            return sale_line_id.price_unit
+        else:
+            return 0.0
---
+++ mrp_unbuild_cust_alu_analytics_valuation/models/stock_picking.py
@@ -0,0 +1,115 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    val_currency_id = fields.Many2one(
+        related="company_id.currency_id",
+        string="Valuation Currency",
+    )
+
+    val_cost = fields.Monetary(
+        currency_field="val_currency_id",
+        digits="Product Price",
+        compute="_compute_val_cost",
+        compute_sudo=True,
+        string="Valuation Cost",
+    )
+
+    val_income = fields.Monetary(
+        currency_field="val_currency_id",
+        digits="Product Price",
+        compute="_compute_val_income",
+        compute_sudo=True,
+        string="Valuation Income",
+    )
+
+    val_margin = fields.Monetary(
+        currency_field="val_currency_id",
+        digits="Product Price",
+        compute="_compute_val_margin",
+        compute_sudo=True,
+        string="Valuation Margin",
+    )
+
+    val_margin_pct = fields.Float(
+        compute="_compute_val_margin",
+        compute_sudo=True,
+        string="Valuation Margin %",
+    )
+
+    val_unbuild_ids = fields.Many2many(
+        comodel_name="mrp.unbuild",
+        compute="_compute_val_unbuild_ids",
+        compute_sudo=True,
+        string="Valuation Involved Unbuilds",
+    )
+
+    def _compute_val_cost(self):
+        pick_out = self.filtered(
+            lambda x: x.picking_type_code == "outgoing" and x.state == "done"
+        )
+        for picking in pick_out:
+            picking.val_cost = sum(
+                move._get_move_cost()
+                for move in picking.move_lines
+            )
+        # TODO replace write() by update()
+        (self - pick_out).write({
+            "val_cost": 0.0,
+        })
+    
+    def _get_compute_val_income_fields(self):
+        """
+        This technique prevents creating a new bridge addon for sales link
+        """
+        fields = []
+        if "sale_id" in self:
+            # sale_stock is already installed
+            fields.append("move_lines.sale_line_id")
+        return fields
+    
+    @api.depends(_get_compute_val_income_fields)
+    def _compute_val_income(self):
+        pick_out = self.filtered(
+            lambda x: x.picking_type_code == "outgoing" and x.state == "done"
+        )
+        for picking in pick_out:
+            # TODO proper UoM conversion
+            picking.val_income = sum(
+                move.product_uom_qty * move._get_move_income_price_unit()
+                for move in picking.move_lines
+            )
+        # TODO replace write() by update()
+        (self - pick_out).write({
+            "val_income": 0.0,
+        })
+
+    @api.depends("val_cost", "val_income")
+    def _compute_val_margin(self):
+        # TODO float_is_zero or float_compare
+        pick_cost = self.filtered(lambda x: x.val_cost)        
+        for picking in pick_cost:
+            picking.val_margin = picking.val_income - picking.val_cost
+            picking.val_margin_pct = picking.val_margin / picking.val_cost
+        # TODO or val_margin = val_income ?
+        # TODO replace write() by update()
+        (self - pick_cost).write({
+            "val_margin": 0.0,
+            "val_margin_pct": 0.0,
+        })
+
+    def _compute_val_unbuild_ids(self):
+        pick_out = self.filtered(
+            lambda x: x.picking_type_code == "outgoing" and x.state == "done"
+        )
+        for picking in pick_out:
+            val_unbuild_ids = self.env["mrp.unbuild"]
+            for ml in picking.move_line_ids.filtered(lambda x: x.state == "done"):
+                val_unbuild_ids |= ml.move_id._get_move_line_cost_unbuild_ids(ml)
+            picking.val_unbuild_ids = val_unbuild_ids
+        (self - pick_out).write({"val_unbuild_ids": False})
