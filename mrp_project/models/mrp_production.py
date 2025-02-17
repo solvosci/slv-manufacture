40c3499ccd6b5eb8ed82d5eec4d57bc164b6a26e
Iria Alonso <iria.alonso@solvos.es>
2025-02-17 11:29:20 +0100

    [IMP] mrp_project: assign project from source MO to child MO When a MO is assigned a project (add, change, or remove), it automatically assigns it to its child MOs, if it has

--- mrp_project/models/mrp_production.py
+++ mrp_project/models/mrp_production.py
@@ -16,3 +16,13 @@ class ManufactureOrder(models.Model):
         if not self.project_id:
             self.project_sequence = 0
 
+    def _assign_project_to_children(self):
+        self.ensure_one()
+        child_mrp = self._get_children()
+        for child in child_mrp:
+            child.project_id = self.project_id
+            child._assign_project_to_children()
+
+    @api.onchange("project_id")
+    def _onchange_project_id_parent(self):
+        self._assign_project_to_children()
