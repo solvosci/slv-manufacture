edf9ddca77a6c83917b68f12bf530776d825e9d1
Iria Alonso <iria.alonso@solvos.es>
2025-02-19 10:18:43 +0100

    [ADD] mrp_inherit_responsible The responsible for a source MO, is the same for the childs MO

---
+++ mrp_inherit_responsible/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "MRP Inherit Responsible",
+    "summary": """
+        The responsible for a source MO, is the same for the MO childs, if it has.
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://github.com/solvosci/slv-manufacture",
+    "depends": ["mrp"],
+    "installable": True,
+}
---
+++ mrp_inherit_responsible/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+The responsible for a source MO, is the same for the MO childs, if it has.
