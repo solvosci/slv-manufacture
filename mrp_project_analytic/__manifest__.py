ce088371b7c29e075b582e7302f51a77a67253e6
Christian Santamaria <christian.santamaria@solvos.es>
2025-03-06 10:17:27 +0100

    [IMP] mrp_project_analytic: add readonly in the unbuilt check

--- mrp_project_analytic/__manifest__.py
+++ mrp_project_analytic/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "15.0.1.4.0",
+    "version": "15.0.1.5.0",
     "category": "Manufacturing",
     "website": "https://github.com/solvosci/slv-manufacture",
     "depends": ["mrp_project", "mrp_account"],
