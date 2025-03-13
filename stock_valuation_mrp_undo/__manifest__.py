23cb97ae6ab6b6cacbcda14c6c8965d0f468d17f
David Alonso // Solvos <david.alonso@solvos.es>
2025-03-13 18:50:53 +0100

    [ADD] stock_valuation_mrp_undo
    Links backing to draft a Production with Stock Valuation modules.
    
    With this bridge, valuation layers and related account moves are removed. Then,
    average prices recomputation in that case is also guaranteed.

---
+++ stock_valuation_mrp_undo/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+{
+    "name": "MRP Production - Enable undo operation - Link with Stock Valuation",
+    "summary": """
+        Links backing to draft a Production with Stock Valuation modules
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://github.com/solvosci/slv-manufacture",
+    "depends": ["mrp_production_undo", "stock_valuation_mrp"],
+}
