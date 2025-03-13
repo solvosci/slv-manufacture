9420167311140ad65fbf1bbf515bb16d5edacb09
David Alonso // Solvos <david.alonso@solvos.es>
2025-03-13 12:56:37 +0100

    [ADD] mrp_production_undo
    Enables undo (back to draft) a complete Production, updating stock inventory.
    
    This is enabled only starting from a "done" state and for users that belong to
    a specific security group. Anyway, this operation can be properly bypassed, if
    such operation is needed within a server process.

---
+++ mrp_production_undo/models/stock_move.py
@@ -0,0 +1,20 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _mrp_check_safe_removal(self):
+        """
+        Override when required
+        """        
+        pass
+
+    def _mrp_unlink_previous_stuff(self):
+        """
+        Override when required
+        """
+        pass
