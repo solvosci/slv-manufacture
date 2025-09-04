1f6fc8091ea6828b59d82c483fcf1421dbaa6dc2
Dante Pereyra <dante.pereyra@solvos.es>
2025-09-04 09:45:03 +0200

    [FIX] mdc_weight_mgmt_xml_import: fix missing translations
    Some translations were missing in the es.po file.

--- mdc_weight_mgmt_xml_import/__manifest__.py
+++ mdc_weight_mgmt_xml_import/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "AGPL-3",
-    "version": "17.0.1.2.0",
+    "version": "17.0.1.2.1",
     "category": "Manufacture",
     "website": "https://github.com/solvosci/manufacture",
     "depends": [
