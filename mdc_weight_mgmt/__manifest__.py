a46e0a2d7eb713f0d5d4aba99e9c019f01f79b0b
Dante Pereyra <dante.pereyra@solvos.es>
2025-09-23 10:33:13 +0200

    [IMP] mdc_weight_mgmt: Changes to make shift optional and support multiple products
    Before this IMP, shift was a required field in the weight record report wizard.
    This change makes the shift field optional, allowing users to generate reports without specifying a shift.
    Additionally, the report template has been updated to handle scenarios where multiple products are selected.

--- mdc_weight_mgmt/__manifest__.py
+++ mdc_weight_mgmt/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "17.0.1.0.2",
+    "version": "17.0.1.1.0",
     "category": "Manufacture",
     "website": "https://github.com/solvosci/manufacture",
     "depends": ["product","maintenance"],
