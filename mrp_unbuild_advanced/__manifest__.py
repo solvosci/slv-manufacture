5015b7d65a5968fad406819b791cce3f91da0f01
Dante Pereyra <dante.pereyra@solvos.es>
2025-09-25 08:22:40 +0200

    [MIG] mrp_unbuild_advanced: Migration to 17.0
    Co-Authored-By: David Alonso <david.alonso@solvos.es>

--- mrp_unbuild_advanced/__manifest__.py
+++ mrp_unbuild_advanced/__manifest__.py
@@ -8,15 +8,23 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.2.11.0",
+    "version": "17.0.1.0.0",
     "category": "Manufacturing",
     "website": "https://github.com/solvosci/slv-manufacture",
-    "depends": ["mrp_tag", "stock_move_action_done_custdate"],
+    "depends": [
+        "mrp_tag",
+        "mrp_unbuild_bom_cust_qty",
+        "stock_move_action_done_custdate"
+    ],
     "data": [
-        "views/assets.xml",
         "security/mrp_unbuild_advanced_security.xml",
         "views/mrp_unbuild_views.xml",
         "views/mrp_unbuild_bom_total_views.xml",
     ],
+    "assets": {
+        "web.assets_backend": [
+            "mrp_unbuild_advanced/static/src/css/tablet_views.css",
+        ],
+    },
     'installable': True,
 }
---
+++ mrp_unbuild_advanced/readme/ROADMAP.md
@@ -0,0 +1,5 @@
+* This addon depends on ``mrp_unbuild_bom_cust_qty`` due to tablet view, that
+  explicity adapts it also for custom quantity views added by that addon.
+  A bridge addon should be depeloped instead.
+* Proper ``calendar`` view display name is lost, from v13. Restore it
+* Uninstall hook for existing updated actions.
