83d76b7284e49150fa04b88fa9716140d26fa6bd
Dante Pereyra <dante.pereyra@solvos.es>
2026-01-08 17:42:28 +0100

    [FIX] mdc_weight_mgmt_xlsx_report: Correct total nominal average calculation
    This commit addresses an issue in the mdc_weight_mgmt_xlsx_report module where
    the total nominal average weight calculation was incorrect. It keeps same logic
    as mdc_weight_mgmt module fix commited previously.

--- mdc_weight_mgmt_xlsx_report/__manifest__.py
+++ mdc_weight_mgmt_xlsx_report/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "AGPL-3",
-    "version": "17.0.1.1.1",
+    "version": "17.0.1.1.2",
     "category": "Manufacture",
     "website": "https://github.com/solvosci/manufacture",
     "depends": [
