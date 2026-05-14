c4520673977a639689f24bc38a0a1b2b674a7107
Dante <dante.pereyra@ciclosmontecastelo.com>
2026-05-14 13:16:25 +0200

    [ADD] quality_control_stock_oca_picking_context
    Add new module for picking context in quality control stock OCA

---
+++ quality_control_stock_oca_picking_context/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2026 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Quality Control Stock OCA Picking Context",
+    "summary": """
+        Adds picking context to quality control stock OCA""",
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://github.com/solvosci/slv-manufacture",
+    "depends": ["quality_control_stock_oca"],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+}
---
+++ quality_control_stock_oca_picking_context/readme/ROADMAP.rst
@@ -0,0 +1,4 @@
+* This it's intended to be added to main `quality_control_stock_oca`` addon
+  as an improvement, and once done, this addon will become unnecessary.
+* Current context is not fully completed, because `reference` field for the
+  inspection is not currently filled.
