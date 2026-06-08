9ba24243f849da73386c534a86f15cc507a3c7ba
Dante Pereyra // Solvos <dante.pereyra@solvos.es>
2026-06-08 10:52:30 +0200

    [ADD] mrp_multi_level_parameter_area
    This addon modifies the product.mrp.area views
    to customize the display of the mrp area tree and form view.

---
+++ mrp_multi_level_parameter_area/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2026 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "MRP Multi-Level Parameter Area",
+    "summary": """
+        This addon modifies the product.mrp.area views
+        to customize the display of the mrp area tree and form view.
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://github.com/solvosci/slv-manufacture",
+    "depends": ["mrp_multi_level"],
+    "data": [
+        "views/product_mrp_area_views.xml",
+    ],
+}
---
+++ mrp_multi_level_parameter_area/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This addon modifies the product.mrp.area views
+to customize the display of the mrp area tree and form view.
