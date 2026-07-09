7911c437025b863e30ee8550226b5a1afce44147
David Alonso // Solvos <david.alonso@solvos.es>
2026-07-09 17:38:33 +0200

    [ADD] mrp_procurement_no_autoconfirm
    Prevent auto confirmation for child MOs generated when confirming ceirtain MO,
    enabling users to manually confirm them later.

---
+++ mrp_procurement_no_autoconfirm/__manifest__.py
@@ -0,0 +1,13 @@
+# © 2026 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+
+{
+    'name': 'MRP - Prevent MO confirmed when comes from a parent MO',
+    'summary': 'For child MOs, prevent auto confirmation',
+    'version': '15.0.1.0.0',
+    'category': 'Manufacturing',
+    'author': 'Solvos',
+    'license': 'LGPL-3',
+    'depends': ['mrp'],
+    'installable': True,
+}
---
+++ mrp_procurement_no_autoconfirm/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+Prevent auto confirmation for child MOs generated when confirming ceirtain MO,
+enabling users to manually confirm them later.
