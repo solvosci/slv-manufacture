34e3740df694ed49e49811ffc996b3dc717d9d2e
David Alonso // Solvos <david.alonso@solvos.es>
2026-09-23 16:36:55 +0200

    [IMP] mrp_production_unbuild_link: cover multi scenario // remove unbuild restrictions
    Before this imp, only one unbuild by MO was considered, and that was
    wrong. Only first unbuild for a MO was considered. In fact, a second
    unbuild was forbidden.
    
    With this improvement, a MO can be unbuilt more than once and properly
    registered.

--- mrp_production_unbuild_link/__manifest__.py
+++ mrp_production_unbuild_link/__manifest__.py
@@ -1,16 +1,15 @@
 # © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
 {
-    "name": "MRP Production - Link to Unbuild",
+    "name": "MRP Production - Link to Unbuilds",
     "summary": """
-        For a MRP Production provides a link to linked Unbuild
+        For a MRP Production provides a link to linked Unbuilds
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "category": "Manufacturing",
     "website": "https://github.com/solvosci/slv-manufacture",
     "depends": ["mrp"],
     "data": ["views/mrp_production_views.xml"],
-    "post_init_hook": "post_init_hook",
 }
